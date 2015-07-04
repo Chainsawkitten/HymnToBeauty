76f3e785604d29e6978fbbccd63911a485877131
Albin Bernhardsson <catscratchesp@gmail.com>
2015-07-04 19:14:16 +0200

    Geometry2D class

---
+++ src/Core/Geometry/Geometry2D.hpp
@@ -0,0 +1,70 @@
+#ifndef GEOMETRY2D_HPP
+#define GEOMETRY2D_HPP
+
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+
+/** @ingroup Core
+ * @{
+ */
+
+/// Interface for renderable 2D geometry.
+class Geometry2D {
+    public:
+        /// A vertex point.
+        struct Vertex {
+                /// Position.
+                glm::vec3 position;
+                /// %Texture coordinate.
+                glm::vec2 textureCoordinate;
+        };
+        
+        /// Destructor
+        virtual ~Geometry2D();
+        
+        /// Get all the vertices.
+        /**
+         * @return Array of vertices
+         */
+        virtual Vertex* Vertices() const = 0;
+        
+        /// Get the number of vertices.
+        /**
+         * @return The number of vertices
+         */
+        virtual unsigned int VertexCount() const = 0;
+        
+        /// Get all the vertex indices.
+        /**
+         * @return Array of vertex indices
+         */
+        virtual unsigned int* Indices() const = 0;
+        
+        /// Get the number of indicies.
+        /**
+         * @return The number of vertex indices.
+         */
+        virtual unsigned int IndexCount() const = 0;
+        
+        /// Get the vertex array.
+        /**
+         * @return The vertex array
+         */
+        GLuint VertexArray() const;
+        
+    protected:
+        /// Generate vertex and index buffers.
+        void GenerateBuffers();
+        
+        /// Generate vertex array.
+        void GenerateVertexArray();
+        
+    private:
+        GLuint vertexBuffer;
+        GLuint indexBuffer;
+        GLuint vertexArray;
+};
+
+/** @} */
+
+#endif
