7bfb917cc515b3e951afcd2d81a5afed04a073a3
Albin Bernhardsson <catscratchesp@gmail.com>
2015-07-04 15:29:18 +0200

    Shader class

---
+++ src/Core/Shader/Shader.cpp
@@ -0,0 +1,22 @@
+#include "Shader.hpp"
+
+Shader::Shader(const char* source, int sourceLength, GLenum shaderType) {
+	this->shaderType = shaderType;
+
+	shader = glCreateShader(shaderType);
+    GLint length = sourceLength;
+	glShaderSource(shader, 1, &source, &length);
+	glCompileShader(shader);
+}
+
+Shader::~Shader() {
+	glDeleteShader(shader);
+}
+
+GLuint Shader::ShaderID() const {
+	return shader;
+}
+
+GLenum Shader::ShaderType() const {
+	return shaderType;
+}
---
+++ src/Core/Shader/Shader.hpp
@@ -0,0 +1,48 @@
+#ifndef SHADER_HPP
+#define SHADER_HPP
+
+#include <gl/glew.h>
+#include <string>
+
+/** @ingroup Core
+ * @{
+ */
+
+/// A GLSL shader.
+/**
+ * Compiles and handles a GLSL shader. Shaders should be linked together into a ShaderProgram.
+ */
+class Shader {
+	public:
+		/// Compile shader.
+		/**
+		 * @param source GLSL code for the shader.
+		 * @param sourceLength Length of the GLSL source code.
+		 * @param shaderType %Shader type. One of GL_COMPUTE_SHADER, GL_VERTEX_SHADER, GL_TESS_CONTROL_SHADER, GL_TESS_EVALUATION_SHADER, GL_GEOMETRY_SHADER, or GL_FRAGMENT_SHADER.
+		 */
+		Shader(const char* source, int sourceLength, GLenum shaderType);
+
+		/// Destructor
+		~Shader();
+
+		/// Get shader ID.
+		/**
+		 * Used when linking a ShaderProgram.
+		 * @return Shader identifier
+		 */
+		GLuint ShaderID() const;
+
+		/// Get shader type.
+		/**
+		 * @return %Shader type. One of GL_COMPUTE_SHADER, GL_VERTEX_SHADER, GL_TESS_CONTROL_SHADER, GL_TESS_EVALUATION_SHADER, GL_GEOMETRY_SHADER, or GL_FRAGMENT_SHADER.
+		 */
+		GLenum ShaderType() const;
+
+	private:
+		GLuint shader;
+		GLenum shaderType;
+};
+
+/** @} */
+
+#endif
