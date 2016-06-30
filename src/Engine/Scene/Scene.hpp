7340aab983ab2878cb97446c188f42eccf267016
Albin Bernhardsson <catscratchesp@gmail.com>
2016-06-30 23:48:05 +0200

    Method to get all components in a scene.

--- src/Engine/Scene/Scene.hpp
+++ src/Engine/Scene/Scene.hpp
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <map>
+#include <typeinfo>
 
 class Entity;
 namespace Component {
@@ -25,6 +26,12 @@ class Scene {
          */
         Entity* CreateEntity();
         
+        /// Gets all components of a specific type.
+        /**
+         * @return A list of pointers to all components of the specified scene.
+         */
+        template<typename T> std::vector<T*>& GetComponents();
+        
         /// Clear the scene of all entities.
         void Clear();
         
@@ -38,3 +45,7 @@ class Scene {
         // Map containing list of components.
         std::map<const std::type_info*, std::vector<Component::SuperComponent*>> components;
 };
+
+template<typename T> inline std::vector<T*>& Scene::GetComponents() {
+    return reinterpret_cast<std::vector<T*>&>(components[&typeid(T*)]);
+}
