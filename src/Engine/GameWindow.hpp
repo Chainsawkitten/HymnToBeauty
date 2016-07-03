c47f262cb63626f56b29552ae5e845cd10047530
Albin Bernhardsson <catscratchesp@gmail.com>
2016-07-03 12:52:22 +0200

    Method to get the size of the game window.

--- src/Engine/GameWindow.hpp
+++ src/Engine/GameWindow.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 
 /// Handles the game window.
 class GameWindow {
@@ -22,6 +23,12 @@ class GameWindow {
 
         /// Render the game.
         void Render();
+        
+        /// Get the size of the window.
+        /**
+         * @return The size of the render area in pixels
+         */
+        glm::vec2 GetSize() const;
 
     private:
         GLFWwindow* window;
