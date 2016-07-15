113f23f44c7a442fd9cefc2830c801fd9a6c4bcf
Albin Bernhardsson <catscratchesp@gmail.com>
2016-07-15 16:54:09 +0200

    Display list of entities.

--- src/Editor/GUI/ResourceList.cpp
+++ src/Editor/GUI/ResourceList.cpp
@@ -6,6 +6,9 @@
 #include <Engine/Font/Font.hpp>
 #include "ABeeZee.ttf.hpp"
 
+#include <Engine/Hymn.hpp>
+#include <Engine/Scene/Scene.hpp>
+
 using namespace GUI;
 
 ResourceList::ResourceList(Widget* parent) : Widget(parent) {
@@ -29,6 +32,14 @@ void ResourceList::Render(const glm::vec2& screenSize) {
     
     font->SetColor(glm::vec3(1.f, 1.f, 1.f));
     font->RenderText("Entities", position, GetSize().x, screenSize);
+    position.y += font->GetHeight();
+    
+    unsigned int id = 0;
+    for (Entity* entity : Hymn().activeScene.GetEntities()) {
+        font->RenderText(("Entity #" + std::to_string(id)).c_str(), position + glm::vec2(20.f, 0.f), GetSize().x, screenSize);
+        position.y += font->GetHeight();
+        ++id;
+    }
 }
 
 glm::vec2 ResourceList::GetSize() const {
