f9e7dc4b2423dcc6d6e1eff3e11da02f486b8657
Albin Bernhardsson <catscratchesp@gmail.com>
2016-07-31 00:21:09 +0200

    Add margin to vec3 editor.

--- src/Editor/GUI/Vec3Editor.cpp
+++ src/Editor/GUI/Vec3Editor.cpp
@@ -9,19 +9,19 @@ Vec3Editor::Vec3Editor(Widget* parent, Font* font) : Widget(parent) {
     this->font = font;
     variable = nullptr;
     
-    xLabel = new Label(this, font, "X:");
+    xLabel = new Label(this, font, "X");
     xLabel->SetPosition(GetPosition());
     
     xEditor = new FloatEditor(this, font);
     xEditor->SetPosition(GetPosition());
     
-    yLabel = new Label(this, font, "Y:");
+    yLabel = new Label(this, font, "Y");
     yLabel->SetPosition(GetPosition());
     
     yEditor = new FloatEditor(this, font);
     yEditor->SetPosition(GetPosition());
     
-    zLabel = new Label(this, font, "Z:");
+    zLabel = new Label(this, font, "Z");
     zLabel->SetPosition(GetPosition());
     
     zEditor = new FloatEditor(this, font);
@@ -58,13 +58,13 @@ void Vec3Editor::SetPosition(const glm::vec2& position) {
     Widget::SetPosition(position);
     
     xLabel->SetPosition(position);
-    xEditor->SetPosition(position + glm::vec2(xLabel->GetSize().x, 0.f));
+    xEditor->SetPosition(position + glm::vec2(20.f, 0.f));
     
     yLabel->SetPosition(position + glm::vec2(0.f, 20.f));
-    yEditor->SetPosition(position + glm::vec2(yLabel->GetSize().x, 20.f));
+    yEditor->SetPosition(position + glm::vec2(20.f, 20.f));
     
     zLabel->SetPosition(position+ glm::vec2(0.f, 40.f));
-    zEditor->SetPosition(position + glm::vec2(zLabel->GetSize().x, 40.f));
+    zEditor->SetPosition(position + glm::vec2(20.f, 40.f));
 }
 
 glm::vec2 Vec3Editor::GetSize() const {
@@ -75,9 +75,9 @@ void Vec3Editor::SetSize(const glm::vec2& size) {
     this->size = size;
     this->size.y = 60.f;
     
-    xEditor->SetSize(glm::vec2(size.x - xLabel->GetSize().x, 20.f));
-    yEditor->SetSize(glm::vec2(size.x - yLabel->GetSize().x, 20.f));
-    zEditor->SetSize(glm::vec2(size.x - zLabel->GetSize().x, 20.f));
+    xEditor->SetSize(glm::vec2(size.x - 20.f, 20.f));
+    yEditor->SetSize(glm::vec2(size.x - 20.f, 20.f));
+    zEditor->SetSize(glm::vec2(size.x - 20.f, 20.f));
 }
 
 void Vec3Editor::SetVec3(glm::vec3* variable) {
