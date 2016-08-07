fc53eae95f4cefd9f0dc69652029253de8aa32f6
Albin Bernhardsson <catscratchesp@gmail.com>
2016-08-07 18:36:27 +0200

    List of textures in hymn.

--- src/Engine/Hymn.cpp
+++ src/Engine/Hymn.cpp
@@ -7,6 +7,7 @@
 #include "Component/Transform.hpp"
 #include "Component/Lens.hpp"
 #include "Geometry/OBJModel.hpp"
+#include "Texture/Texture2D.hpp"
 
 using namespace std;
 
@@ -24,12 +25,19 @@ void ActiveHymn::Clear() {
     path = "";
     activeScene.Clear();
     
+    entityNumber = 1U;
+    
     for (Geometry::OBJModel* model : models) {
         delete model;
     }
     models.clear();
     modelNumber = 0U;
-    entityNumber = 1U;
+    
+    for (Texture2D* texture : textures) {
+        delete texture;
+    }
+    textures.clear();
+    textureNumber = 0U;
     
     Entity* camera = activeScene.CreateEntity("Camera");
     Component::Transform* cameraTransform = camera->AddComponent<Component::Transform>();
