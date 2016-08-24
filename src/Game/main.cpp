3c159f1857611fd1a6ab2eae12cd9ad9088342ff
Albin Bernhardsson <catscratchesp@gmail.com>
2016-08-24 17:03:52 +0200

    Main game loop with target FPS.

--- src/Game/main.cpp
+++ src/Game/main.cpp
@@ -4,6 +4,7 @@
 #include <Engine/Manager/Managers.hpp>
 #include <Engine/Hymn.hpp>
 #include <Engine/Util/Log.hpp>
+#include <thread>
 
 int main() {
     if (!glfwInit())
@@ -19,10 +20,27 @@ int main() {
     
     Hymn().Load(".");
     
+    // Main loop.
+    double targetFPS = 60.0;
+    double lastTime = glfwGetTime();
+    double lastTimeRender = glfwGetTime();
     while (!window->ShouldClose()) {
+        double deltaTime = glfwGetTime() - lastTime;
+        lastTime = glfwGetTime();
+        
         window->Update();
+        Hymn().Update(static_cast<float>(deltaTime));
         Hymn().Render();
+        
+        // Swap buffers and wait until next frame.
         window->SwapBuffers();
+        
+        long wait = static_cast<long>((1.0 / targetFPS + lastTimeRender - glfwGetTime()) * 1000000.0);
+        if (wait > 0)
+            std::this_thread::sleep_for(std::chrono::microseconds(wait));
+        lastTimeRender = glfwGetTime();
+        
+        // Get input.
         glfwPollEvents();
     }
     
