b83ee80d5176888948cdae1a2512e902ce06062f
Albin Bernhardsson <catscratchesp@gmail.com>
2016-09-20 23:41:10 +0200

    Method to save/load vec2 to/from Json.

--- src/Engine/Util/Json.cpp
+++ src/Engine/Util/Json.cpp
@@ -1,6 +1,17 @@
 #include "Json.hpp"
 
 namespace Json {
+    Value SaveVec2(const glm::vec2& value) {
+        Value node;
+        node["x"] = value.x;
+        node["y"] = value.y;
+        return node;
+    }
+    
+    glm::vec2 LoadVec2(const Value& value) {
+        return glm::vec2(value.get("x", 0.f).asFloat(), value.get("y", 0.f).asFloat());
+    }
+    
     Value SaveVec3(const glm::vec3& value) {
         Value node;
         node["x"] = value.x;
