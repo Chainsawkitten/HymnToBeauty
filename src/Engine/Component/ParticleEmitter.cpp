135e4e74073d39831125702a9da02bb03221fd5a
Albin Bernhardsson <catscratchesp@gmail.com>
2016-09-21 00:46:03 +0200

    Fix particle emitter loading.

--- src/Engine/Component/ParticleEmitter.cpp
+++ src/Engine/Component/ParticleEmitter.cpp
@@ -33,18 +33,18 @@ Json::Value ParticleEmitter::Save() const {
 
 void ParticleEmitter::Load(const Json::Value& node) {
     particleType.textureIndex = node.get("textureIndex", 0).asInt();
-    particleType.minVelocity = Json::LoadVec3("minVelocity");
-    particleType.maxVelocity = Json::LoadVec3("maxVelocity");
+    particleType.minVelocity = Json::LoadVec3(node["minVelocity"]);
+    particleType.maxVelocity = Json::LoadVec3(node["maxVelocity"]);
     particleType.minLifetime = node.get("minLifetime", 0.f).asFloat();
     particleType.maxLifetime = node.get("maxLifetime", 0.f).asFloat();
-    particleType.minSize = Json::LoadVec2("minSize");
-    particleType.maxSize = Json::LoadVec2("maxSize");
+    particleType.minSize = Json::LoadVec2(node["minSize"]);
+    particleType.maxSize = Json::LoadVec2(node["maxSize"]);
     particleType.uniformScaling = node.get("uniformScaling", false).asBool();
     particleType.startAlpha = node.get("startAlpha", 0.f).asFloat();
     particleType.midAlpha = node.get("midAlpha", 1.f).asFloat();
     particleType.endAlpha = node.get("endAlpha", 0.f).asFloat();
-    particleType.color = Json::LoadVec3("color");
-    size = Json::LoadVec3("size");
+    particleType.color = Json::LoadVec3(node["color"]);
+    size = Json::LoadVec3(node["size"]);
     minEmitTime = node.get("minEmitTime", 0.03).asDouble();
     maxEmitTime = node.get("maxEmitTime", 0.03).asDouble();
     emitterType = static_cast<EmitterType>(node.get("emitterType", 0).asInt());
