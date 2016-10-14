d60fc411096503de437bb4a268db852ccb23e5df
Albin Bernhardsson <catscratchesp@gmail.com>
2016-10-14 16:41:38 +0200

    Add debug drawing point

--- src/Engine/Manager/DebugDrawingManager.cpp
+++ src/Engine/Manager/DebugDrawingManager.cpp
@@ -7,3 +7,12 @@ DebugDrawingManager::DebugDrawingManager() {
 DebugDrawingManager::~DebugDrawingManager() {
     
 }
+
+void DebugDrawingManager::AddPoint(const glm::vec3& position, const glm::vec3& color, float duration, bool depthTesting) {
+    Point point;
+    point.position = position;
+    point.color = color;
+    point.duration = duration;
+    point.depthTesting = depthTesting;
+    points.push_back(point);
+}
--- src/Engine/Manager/DebugDrawingManager.hpp
+++ src/Engine/Manager/DebugDrawingManager.hpp
@@ -1,12 +1,35 @@
 #pragma once
 
+#include <GL/gl.h>
+#include <glm/glm.hpp>
+#include <vector>
+
 /// Debug drawing facilities.
 class DebugDrawingManager {
     friend class Hub;
     
+    public:
+        /// Add a point to the scene.
+        /**
+         * @param position World position of the point.
+         * @param color Color of the point.
+         * @param duration How long the point should stay in the scene (in seconds).
+         * @param depthTesting Whether to enable depth testing.
+         */
+        void AddPoint(const glm::vec3& position, const glm::vec3& color, float duration = 0.f, bool depthTesting = true);
+        
     private:
         DebugDrawingManager();
         ~DebugDrawingManager();
         DebugDrawingManager(DebugDrawingManager const&) = delete;
         void operator=(DebugDrawingManager const&) = delete;
+        
+        // Points.
+        struct Point {
+            glm::vec3 position;
+            glm::vec3 color;
+            float duration;
+            bool depthTesting;
+        };
+        std::vector<Point> points;
 };
