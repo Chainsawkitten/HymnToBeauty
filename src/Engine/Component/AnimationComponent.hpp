88b221e2c92a919810f7814432246de8b793528f
LarsIngo <ivarbjorling@hotmail.com>
2016-11-08 11:08:59 +0100

    Added animation component

---
+++ src/Engine/Component/AnimationComponent.hpp
@@ -0,0 +1,71 @@
+#pragma once
+
+#include "SuperComponent.hpp"
+#include <glm/glm.hpp>
+
+namespace Component {
+    /// %Component providing transformation.
+    class Transform : public SuperComponent {
+        public:
+            /// Create new %Transform component.
+            /**
+             * @param entity Pointer to which entity this component corresponds.
+             */
+            Transform(Entity* entity);
+            
+            /// Save the component.
+            /**
+             * @return JSON value to be stored on disk.
+             */
+            Json::Value Save() const override;
+            
+            /// Load component from JSON node.
+            /**
+             * @param node JSON node to load from.
+             */
+            void Load(const Json::Value& node) override;
+            
+            /// Get the model matrix.
+            /**
+             * @return The model matrix.
+             */
+            glm::mat4 GetModelMatrix() const;
+            
+            /// Get orientation matrix.
+            /**
+             * @return The entity's orientation matrix.
+             */
+            glm::mat4 GetOrientation() const;
+            
+            /// Get orientation matrix (for camera).
+            /**
+             * Calculates the orientation matrix as if the entity was a camera.
+             * @return The entity's orientation matrix.
+             */
+            glm::mat4 GetCameraOrientation() const;
+            
+            /// Get direction of the entity.
+            /**
+             * @return The entity's direction.
+             */
+            glm::vec3 GetDirection() const;
+            
+            /// Position in the world.
+            /**
+             * Default: 0.f, 0.f, 0.f
+             */
+            glm::vec3 position = glm::vec3(0.f, 0.f, 0.f);
+            
+            /// Scale.
+            /**
+             * Default: 1.f, 1.f, 1.f
+             */
+            glm::vec3 scale = glm::vec3(1.f, 1.f, 1.f);
+            
+            /// Rotation (yaw, pitch, roll in degrees).
+            /**
+             * Default: 0.f, 0.f, 0.f
+             */
+            glm::vec3 rotation = glm::vec3(0.f, 0.f, 0.f);
+    };
+}
--- src/Engine/Geometry/Animation.hpp
+++ src/Engine/Geometry/Animation.hpp
@@ -8,62 +8,64 @@
 
 struct aiAnimation;
 
-/// An animation loaded from a file.
-class Animation {
-    public:
-        /// Animation channel representing rotation, position and scale transformations.
-        struct AnimChannel {
-            struct QuatKey {
-                double time;
-                aiQuaternion value;
-            };
-            struct VectorKey {
-                double time;
-                glm::vec3 value;
+namespace Geometry {
+    /// An animation loaded from a file.
+    class Animation {
+        public:
+            /// Animation channel representing rotation, position and scale transformations.
+            struct AnimChannel {
+                struct QuatKey {
+                    double time;
+                    aiQuaternion value;
+                };
+                struct VectorKey {
+                    double time;
+                    glm::vec3 value;
+                };
+
+                std::size_t FindRotKey(float animationTime) const;
+                std::size_t FindPosKey(float animationTime) const;
+                std::size_t FindSclKey(float animationTime) const;
+
+                std::vector<QuatKey> rotKeys;
+                std::vector<VectorKey> posKeys;
+                std::vector<VectorKey> sclKeys;
+                std::string trgNodeName = "";
             };
 
-            std::size_t FindRotKey(float animationTime) const;
-            std::size_t FindPosKey(float animationTime) const;
-            std::size_t FindSclKey(float animationTime) const;
+            /// Create new empty animation.
+            /**
+             * The created animation has to be loaded later using Load.
+             */
+            Animation();
 
-            std::vector<QuatKey> rotKeys;
-            std::vector<VectorKey> posKeys;
-            std::vector<VectorKey> sclKeys;
-            std::string trgNodeName = "";
+            /// Create new animation.
+            /**
+             * @param aScene Pointer to assimp animation.
+             */
+            Animation(const aiAnimation* aAnimation);
+
+            /// Destructor.
+            ~Animation();
+
+            /// Load animation.
+            /**
+             * @param aScene Pointer to assimp animation.
+             */
+            void Load(const aiAnimation* aAnimation);
+
+            //TODO COMMENTS
+            const AnimChannel* FindChannel(const std::string& nodeName) const;
+
+            static void CalcInterpolatedScaling(glm::vec3& scaling, float animationTime, const AnimChannel* channel);
+            static void CalcInterpolatedRotation(aiQuaternion& rotation, float animationTime, const Animation::AnimChannel* channel);
+            static void CalcInterpolatedPosition(glm::vec3& translation, float animationTime, const Animation::AnimChannel* channel);
+
+            std::string name;
+            double duration;
+            double ticksPerSecond;
+        private:
+            //std::map<std::string, std::size_t> channelIndexMap; 
+            std::vector<AnimChannel> channels;
         };
-
-        /// Create new empty animation.
-        /**
-         * The created animation has to be loaded later using Load.
-         */
-        Animation();
-
-        /// Create new animation.
-        /**
-         * @param aScene Pointer to assimp animation.
-         */
-        Animation(const aiAnimation* aAnimation);
-
-        /// Destructor.
-        ~Animation();
-
-        /// Load animation.
-        /**
-         * @param aScene Pointer to assimp animation.
-         */
-        void Load(const aiAnimation* aAnimation);
-
-        //TODO COMMENTS
-        const AnimChannel* FindChannel(const std::string& nodeName) const;
-
-        static void CalcInterpolatedScaling(glm::vec3& scaling, float animationTime, const AnimChannel* channel);
-        static void CalcInterpolatedRotation(aiQuaternion& rotation, float animationTime, const Animation::AnimChannel* channel);
-        static void CalcInterpolatedPosition(glm::vec3& translation, float animationTime, const Animation::AnimChannel* channel);
-
-        std::string name;
-        double duration;
-        double ticksPerSecond;
-    private:
-        //std::map<std::string, std::size_t> channelIndexMap; 
-        std::vector<AnimChannel> channels;
-    };
+}
