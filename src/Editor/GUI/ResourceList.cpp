8bec1957f2d4c6bf9a1481fabf55e42bc2b77d8c
Pontus Björkberg <pontus.bjorkberg@hotmail.com>
2017-02-06 15:19:51 +0100

    Made the resource window stick to the bottom of the screen and the other editors stick to the right

--- src/Editor/GUI/ResourceList.cpp
+++ src/Editor/GUI/ResourceList.cpp
@@ -8,6 +8,7 @@
 #include <Engine/Hymn.hpp>
 #include <Engine/Scene/Scene.hpp>
 #include <Engine/Entity/Entity.hpp>
+#include <Engine/MainWindow.hpp>
 #include <imgui.h>
 
 using namespace GUI;
@@ -16,12 +17,14 @@ void ResourceList::Show() {
     ImGui::Begin("Resources");
     
     // Entities.
+    Entity* selected_entity = nullptr;
     if (ImGui::TreeNode("Entities")) {
         if (ImGui::Button("Add entity"))
             Hymn().activeScene.CreateEntity("Entity #" + std::to_string(Hymn().entityNumber++));
         
         for (Entity* entity : Hymn().activeScene.GetEntities()) {
             if (ImGui::Selectable(entity->name.c_str())) {
+                selected_entity = entity;
                 entityEditors[entity].SetVisible(true);
                 entityEditors[entity].SetEntity(entity);
             }
@@ -36,15 +39,9 @@ void ResourceList::Show() {
         
         ImGui::TreePop();
     }
-    
-    // Entity editors.
-    for (Entity* entity : Hymn().activeScene.GetEntities()) {
-        if (entityEditors[entity].IsVisible()) {
-            entityEditors[entity].Show();
-        }
-    }
-    
+        
     // Models.
+    Geometry::Model* selected_model = nullptr;
     if (ImGui::TreeNode("Models")) {
         if (ImGui::Button("Add rigged model")) {
             Geometry::Model* model = new Geometry::RiggedModel();
@@ -60,6 +57,7 @@ void ResourceList::Show() {
         for (auto it = Hymn().models.begin(); it != Hymn().models.end(); ++it) {
             Geometry::Model* model = *it;
             if (ImGui::Selectable(model->name.c_str())) {
+                selected_model = model;
                 modelEditors[model].SetVisible(true);
                 modelEditors[model].SetModel(model);
             }
@@ -77,15 +75,9 @@ void ResourceList::Show() {
         
         ImGui::TreePop();
     }
-    
-    // Model editors.
-    for (Geometry::Model* model : Hymn().models) {
-        if (modelEditors[model].IsVisible()) {
-            modelEditors[model].Show();
-        }
-    }
-    
+        
     // Textures.
+    Texture2D* selected_texture = nullptr;
     if (ImGui::TreeNode("Textures")) {
         if (ImGui::Button("Add texture")) {
             Texture2D* texture = new Texture2D();
@@ -96,6 +88,7 @@ void ResourceList::Show() {
         for (auto it = Hymn().textures.begin(); it != Hymn().textures.end(); ++it) {
             Texture2D* texture = *it;
             if (ImGui::Selectable(texture->name.c_str())) {
+                selected_texture = texture;
                 textureEditors[texture].SetVisible(true);
                 textureEditors[texture].SetTexture(texture);
             }
@@ -114,14 +107,8 @@ void ResourceList::Show() {
         ImGui::TreePop();
     }
     
-    // Texture editors.
-    for (Texture2D* texture : Hymn().textures) {
-        if (textureEditors[texture].IsVisible()) {
-            textureEditors[texture].Show();
-        }
-    }
-    
     // Sounds.
+    Audio::SoundBuffer* selected_sound = nullptr;
     if (ImGui::TreeNode("Sounds")) {
         if (ImGui::Button("Add sound")) {
             Audio::SoundBuffer* sound = new Audio::SoundBuffer();
@@ -132,6 +119,7 @@ void ResourceList::Show() {
         for (auto it = Hymn().sounds.begin(); it != Hymn().sounds.end(); ++it) {
             Audio::SoundBuffer* sound = *it;
             if (ImGui::Selectable(sound->name.c_str())) {
+                selected_sound = sound;
                 soundEditors[sound].SetVisible(true);
                 soundEditors[sound].SetSound(sound);
             }
@@ -149,14 +137,84 @@ void ResourceList::Show() {
         
         ImGui::TreePop();
     }
-    
-    // Sound editors.
-    for (Audio::SoundBuffer* sound : Hymn().sounds) {
-        if (soundEditors[sound].IsVisible()) {
-            soundEditors[sound].Show();
+
+    ImVec2 size(MainWindow::GetInstance()->GetSize().x, MainWindow::GetInstance()->GetSize().y);
+    ImGui::SetNextWindowPos(ImVec2(size.x - 250, 0));
+    ImGui::SetNextWindowSize(ImVec2(250, size.y));
+
+    bool no_selection = selected_entity == nullptr && selected_texture == nullptr && selected_sound == nullptr && selected_model == nullptr;
+
+    // Entity editors.
+    if (!no_selection) {
+        for (Entity* entity : Hymn().activeScene.GetEntities()) {
+            if (entityEditors[entity].IsVisible()) {
+                if(entity != selected_entity)
+                    entityEditors[entity].SetVisible(false);
+                else entityEditors[entity].Show();
+            } 
         }
     }
-    
+    else{
+        for (Entity* entity : Hymn().activeScene.GetEntities()) {
+            if (entityEditors[entity].IsVisible()) {
+                entityEditors[entity].Show();
+            }
+        }
+    }
+
+    // Model editors.
+    if (!no_selection) {
+        for (Geometry::Model* models : Hymn().models) {
+            if (modelEditors[models].IsVisible()) {
+                if (models != selected_model)
+                    modelEditors[models].SetVisible(false);
+                else modelEditors[models].Show();
+            }
+        }
+    }
+    else{
+        for (Geometry::Model* model : Hymn().models) {
+            if (modelEditors[model].IsVisible()) {
+                modelEditors[model].Show();
+            }
+        }
+    }
+
+    // Texture editors.
+    if (!no_selection) {
+        for (Texture2D* texture : Hymn().textures) {
+            if (textureEditors[texture].IsVisible()) {
+                if (texture != selected_texture)
+                    textureEditors[texture].SetVisible(false);
+                else textureEditors[texture].Show();
+            }
+        }
+    }
+    else{
+        for (Texture2D* texture : Hymn().textures) {
+            if (textureEditors[texture].IsVisible()) {
+                textureEditors[texture].Show();
+            }
+        }
+    }
+
+    // Sound editors.
+    if (!no_selection) {
+        for (Audio::SoundBuffer* sound : Hymn().sounds) {
+            if (soundEditors[sound].IsVisible()) {
+                if (sound != selected_sound)
+                    soundEditors[sound].SetVisible(false);
+                else soundEditors[sound].Show();
+            }
+        }
+    }
+    else{
+        for (Audio::SoundBuffer* sound : Hymn().sounds) {
+            if (soundEditors[sound].IsVisible()) {
+                soundEditors[sound].Show();
+            }
+        }
+    }
     ImGui::End();
 }
 
