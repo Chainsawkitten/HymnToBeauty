25c9c339195f7299dce8acf8ebea9c51fbe2f910
Albin Bernhardsson <catscratchesp@gmail.com>
2017-03-18 13:09:52 +0100

    Fix Skeleton indentation

--- src/Engine/Geometry/Skeleton.hpp
+++ src/Engine/Geometry/Skeleton.hpp
@@ -19,35 +19,35 @@ class Skeleton {
          * The created skeleton has to be loaded later using Load.
          */
         Skeleton();
-
+        
         /// Create new skeleton.
         /**
          * @param aScene Pointer to assimp scene.
          */
         Skeleton(const aiScene* aScene);
-
+        
         /// Destructor.
         ~Skeleton();
-
+        
         /// Load skeleton.
         /**
          * @param aScene Pointer to assimp scene.
          */
         void Load(const aiScene* aScene);
-
+        
         /// Get bone index.
         /**
          * @param name Bone name.
          * @return Bone index, if not found -1.
          */
         std::size_t FindBoneIndex(const std::string& name);
-
+        
         /// Get number of bones.
         /**
          * @return Number of bones.
          */
         std::size_t GetNumBones() const;
-
+        
         /// Animate skeleton.
         /**
          * Use GetFinalTransformations after animation to get matrices.
@@ -55,27 +55,27 @@ class Skeleton {
          * @param timeInSeconds Time to find animation frame.
          */
         void Animate(const Geometry::Animation* animation, const float timeInSeconds);
-
+        
         /// Update skeleton to bind pose.
         /**
          * Use GetFinalTransformations after animation to get matrices.
          */
         void BindPose();
-
+        
         /// Get vector of transformations of skeleton.
         /**
          * Final transformations are updated when skeleton is animated.
          * @return Vector of bone transformations.
          */
         const std::vector<glm::mat4>& GetFinalTransformations() const;
-
+        
         /// Get vector of inverse transpose transformations of skeleton.
         /**
          * Final transformations are updated when skeleton is animated.
          * @return Vector of bone inverse transpose transformations.
          */
         const std::vector<glm::mat3>& GetFinalTransformationsIT() const;
-
+        
     private:
         struct Node {
             std::string name;
@@ -83,11 +83,11 @@ class Skeleton {
             Node* parent;
             std::vector<Node> children;
         };
-
+        
         static void LoadNodeTree(aiNode* aNode, Node* node, Node* parentNode);
         void ReadNodeHeirarchy(const Geometry::Animation* animation, float animationTime, Node* node, const glm::mat4& parentTransform);
         const glm::mat4* FindBone(const std::string& name) const;
-
+        
         glm::mat4 globalInverseTransform;
         Node rootNode;
         std::vector<glm::mat4> bones;
