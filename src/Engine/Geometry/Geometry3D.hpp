7960b8ab50960708751b5e7924644b76174f2467
Albin Bernhardsson <catscratchesp@gmail.com>
2017-03-18 13:01:10 +0100

    Fix Geometry3D indentation

--- src/Engine/Geometry/Geometry3D.hpp
+++ src/Engine/Geometry/Geometry3D.hpp
@@ -13,7 +13,7 @@ namespace Geometry {
                 STATIC = 0, ///< Default3D vertex layout (Default3D.vert).
                 SKIN ///< Skinning vertex layout (Skinning.vert).
             };
-
+            
             /// Destructor.
             virtual ~Geometry3D();
 
@@ -22,32 +22,32 @@ namespace Geometry {
              * @return The vertex array.
              */
             GLuint GetVertexArray() const;
-
+            
             /// Get number of indices.
             /**
              * @return Index count.
              */
             unsigned int GetIndexCount() const;
-
+            
             /// Get the axis-aligned bounding box around the geometry.
             /**
              * @return Local space axis-aligned bounding box around the geometry.
              */
             const Physics::AxisAlignedBoundingBox& GetAxisAlignedBoundingBox() const;
-
+            
             /// Get geometry type.
             /**
              * @return Type.
              */
             virtual Type GetType() const = 0;
-
+            
         protected:
             /// Generate vertex buffer.
             /**
              * @param vertexBuffer Vertex buffer.
              */
             virtual void GenerateVertexBuffer(GLuint& vertexBuffer) = 0;
-
+            
             /// Generate vertex array.
             /**
              * @param vertexBuffer Vertex buffer.
@@ -55,7 +55,7 @@ namespace Geometry {
              * @param vertexArray Vertex array.
              */
             virtual void GenerateVertexArray(const GLuint vertexBuffer, const GLuint indexBuffer, GLuint& vertexArray) = 0;
-
+            
             /// Generate index buffer.
             /**
              * @param indexData Pointer to array of indices.
@@ -63,22 +63,22 @@ namespace Geometry {
              * @param indexBuffer Index buffer.
              */
             void GenerateIndexBuffer(unsigned int* indexData, unsigned int indexCount, GLuint& indexBuffer);
-
+            
             /// Create local space axis-aligned bounding box around the geometry.
             /**
              * @param positions Vector of vertex positions.
              */
             void CreateAxisAlignedBoundingBox(const std::vector<glm::vec3*>& positions);
-
+            
             /// Vertex buffer.
             GLuint vertexBuffer;
-
+            
             /// Index buffer.
             GLuint indexBuffer;
-
+            
             /// Vertex array.
             GLuint vertexArray;
-
+            
         private:
             Physics::AxisAlignedBoundingBox axisAlignedBoundingBox;
             unsigned int indexCount = 0;
