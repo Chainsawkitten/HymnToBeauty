4948ea7bb656c35f181c18ceb6a670fd3b14eb40
Albin Bernhardsson <catscratchesp@gmail.com>
2017-05-05 13:56:17 +0200

    Fix indentation

--- src/Engine/Manager/ParticleManager.hpp
+++ src/Engine/Manager/ParticleManager.hpp
@@ -20,29 +20,29 @@ class ParticleManager {
     public:
         /// A particle in the particle system.
         struct Particle {
-                /// Position.
-                glm::vec3 worldPos;
-                
-                /// Size.
-                glm::vec2 size;
-                
-                /// Life (in seconds).
-                float life;
-                
-                /// Lifetime (in seconds).
-                float lifetime;
-                
-                /// Initial velocity.
-                glm::vec3 velocity;
-                
-                /// Start, mid and end of life alpha of particle.
-                glm::vec3 alpha;
-                
-                /// Color of the particle.
-                glm::vec3 color;
-                
-                /// Texture index (for the texture atlas, left to right, top to bottom indexing)
-                float textureIndex;
+            /// Position.
+            glm::vec3 worldPos;
+            
+            /// Size.
+            glm::vec2 size;
+            
+            /// Life (in seconds).
+            float life;
+            
+            /// Lifetime (in seconds).
+            float lifetime;
+            
+            /// Initial velocity.
+            glm::vec3 velocity;
+            
+            /// Start, mid and end of life alpha of particle.
+            glm::vec3 alpha;
+            
+            /// Color of the particle.
+            glm::vec3 color;
+            
+            /// Texture index (for the texture atlas, left to right, top to bottom indexing)
+            float textureIndex;
         };
         
         /// Get the maximum amount of particles.
