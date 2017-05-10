db291a3f8f8baae8d5775ee86b43a1f674eac297
NewUserName <danielpejcinovski@hotmail.com>
2017-05-10 02:57:40 +0200

    Loads of ways to sample value from curve.

--- src/Editor/GUI/BezierWidget.cpp
+++ src/Editor/GUI/BezierWidget.cpp
@@ -24,9 +24,345 @@ Curve::Curve() {
     points.push_back(glm::vec2(1.0f, 0.00f));
 }
 
+float Curve::Sample(float x){
+    // If we sample outside, return edge y.
+    if(x < 0.f)
+        return points[0].y;
+    else if(x > 1.f)
+        return points[points.size()-1].y;
+
+    int numberOfPoints = points.size();
+    int leftIndex = 0;
+    int rightIndex = 0;
+
+    // Find the left and right indices.
+    for(int i = 0; i < numberOfPoints; i++) {
+        if(x >= points[i].x && x <= points[i+1].x){
+            leftIndex = i;
+            rightIndex = leftIndex+1;
+            break;
+        }
+
+    }
+
+    const float pi = glm::pi<float>();
+    const float piHalf = glm::pi<float>() / 2.f;
+    float rightCoefficient = (x - points[leftIndex].x) / (points[rightIndex].x - points[leftIndex].x);
+    float leftCoefficient = 1.f - rightCoefficient;
+
+    switch (curveType) {
+        case CurveType::LINEAR:
+            return leftCoefficient*points[leftIndex].y + rightCoefficient*points[rightIndex].y;
+        break;
+
+        case CurveType::QUADIN:
+            leftCoefficient = leftCoefficient*leftCoefficient;
+            rightCoefficient = 1.f-leftCoefficient;
+            return leftCoefficient*points[leftIndex].y + rightCoefficient*points[rightIndex].y;
+        break;
+
+        case CurveType::QUADOUT:
+            leftCoefficient = -leftCoefficient*(leftCoefficient-2.f);
+            rightCoefficient = 1.f - leftCoefficient;
+            return leftCoefficient*points[leftIndex].y + rightCoefficient*points[rightIndex].y;
+        break;
+
+        case CurveType::QUADINOUT:
+            if(leftCoefficient < 0.5f) {
+                leftCoefficient = 2*leftCoefficient*leftCoefficient;
+            } else {
+                leftCoefficient = (-2.f * leftCoefficient * leftCoefficient) + (4*leftCoefficient) - 1.f;
+            }
+            rightCoefficient = 1.f - leftCoefficient;
+            return leftCoefficient*points[leftIndex].y + rightCoefficient*points[rightIndex].y;
+        break;
+
+        case CurveType::CUBICIN:
+            leftCoefficient = leftCoefficient * leftCoefficient * leftCoefficient;
+            rightCoefficient = 1.f - leftCoefficient;
+            return leftCoefficient*points[leftIndex].y + rightCoefficient*points[rightIndex].y;
+        break;
+
+        case CurveType::CUBICOUT:
+            leftCoefficient = leftCoefficient - 1.f;
+            leftCoefficient = leftCoefficient * leftCoefficient * leftCoefficient + 1.f;
+            rightCoefficient = 1.f - leftCoefficient;
+            return leftCoefficient*points[leftIndex].y + rightCoefficient*points[rightIndex].y;
+        break;
+
+        case CurveType::CUBICINOUT:
+            if(leftCoefficient < 0.5f) {
+                leftCoefficient = 4 * leftCoefficient * leftCoefficient * leftCoefficient;
+            } else {
+                float f = ((2.f * leftCoefficient) - 2.f);
+                leftCoefficient = 0.5f * f * f * f  + 1.f;
+            }
+            rightCoefficient = 1.f - leftCoefficient;
+            return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+        break;
+
+        case CurveType::QUARTIN:
+            leftCoefficient = leftCoefficient*leftCoefficient*leftCoefficient*leftCoefficient;
+            rightCoefficient = 1.f - leftCoefficient;
+            return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+        break;
+
+        case CurveType::QUARTOUT: { // Due to case label.
+            float f = leftCoefficient-1.f;
+            leftCoefficient = f*f*f*(1.f - leftCoefficient) + 1.f;
+            rightCoefficient = 1.f - leftCoefficient;
+            return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+        }
+        break;
+
+        case CurveType::QUARTINOUT:
+            if(leftCoefficient < 0.5f) {
+                leftCoefficient = 8.f*leftCoefficient*leftCoefficient*leftCoefficient*leftCoefficient;
+            } else {
+                float f = (leftCoefficient - 1.f);
+                leftCoefficient = -8.f * f * f * f * f + 1.f;
+            }
+            rightCoefficient = 1.f - leftCoefficient;
+            return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+        break;
+
+        case CurveType::QUINTIN:
+            leftCoefficient = leftCoefficient * leftCoefficient * leftCoefficient * leftCoefficient * leftCoefficient;
+            rightCoefficient = 1.f - leftCoefficient;
+            return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+        break;
+
+        case CurveType::QUINTOUT:
+            leftCoefficient = (leftCoefficient - 1.f);
+            leftCoefficient = leftCoefficient * leftCoefficient * leftCoefficient * leftCoefficient * leftCoefficient + 1.f;
+            rightCoefficient = 1.f - leftCoefficient;
+            return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+        break;
+
+        case CurveType::QUINTINOUT:
+            if( leftCoefficient < 0.5f){
+                leftCoefficient = 16 * leftCoefficient * leftCoefficient * leftCoefficient * leftCoefficient * leftCoefficient;
+            } else {
+                float f = ((2.f * leftCoefficient) - 2.f);
+                leftCoefficient = 0.5f * f * f  * f * f * f + 1.f;
+            }
+            rightCoefficient = 1.f - leftCoefficient;
+            return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+        break;
+
+        case CurveType::SINEIN:
+            leftCoefficient = std::sin((leftCoefficient - 1.f)*piHalf) + 1.f;
+            rightCoefficient = 1.f - leftCoefficient;
+            return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+        break;
+
+        case CurveType::SINEOUT:
+            leftCoefficient = std::sin(leftCoefficient * piHalf);
+            rightCoefficient = 1.f - leftCoefficient;
+            return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+        break;
+
+        case CurveType::SINEINOUT:
+            leftCoefficient = 0.5f  *(1.f - std::cos(leftCoefficient*pi));
+            rightCoefficient = 1.f - leftCoefficient;
+            return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+        break;
+
+        case CurveType::EXPOIN:
+            // Check if we are too close to zero.
+            if( leftCoefficient < 0.0001f && leftCoefficient > -0.0001f ) {
+                rightCoefficient = 1.f - leftCoefficient;
+                return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+            } else {
+                leftCoefficient = std::pow(2.f, 10.f * (leftCoefficient - 1.f));
+                rightCoefficient = 1.f - leftCoefficient;
+                return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+            }
+        break;
+
+        case CurveType::EXPOOUT:
+            if( leftCoefficient < 1.0001f && leftCoefficient > (1.0001f - 0.0001f)  ) {
+                rightCoefficient = 1.f - leftCoefficient;
+                return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+            }
+            leftCoefficient = 1.f - pow(2.f,-10*leftCoefficient);
+            rightCoefficient = 1.f - leftCoefficient;
+            return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+        break;
+
+        case CurveType::EXPOINOUT:
+            if( (leftCoefficient < 1.0001f && leftCoefficient > (1.0001f - 0.0001f)) ||  (leftCoefficient < 0.0001f && leftCoefficient > -0.0001f ) ){
+                rightCoefficient = 1.f - leftCoefficient;
+                return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+            }
+
+            if(leftCoefficient < 0.5f) {
+                leftCoefficient = 0.5f * pow(2.f, (20.f*leftCoefficient) - 10.f);
+            } else {
+                leftCoefficient = -0.5f * pow(2.f, (-20.f*leftCoefficient) + 10.f) + 1.f;
+            }
+            rightCoefficient = 1.f - leftCoefficient;
+            return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+        break;
+
+        case CurveType::CIRCIN:
+            leftCoefficient = 1.f - std::sqrt(1.f - (leftCoefficient*leftCoefficient));
+            rightCoefficient = 1.f - leftCoefficient;
+            return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+        break;
+
+        case CurveType::CIRCOUT:
+            leftCoefficient = std::sqrt((2.f - leftCoefficient) * leftCoefficient);
+            rightCoefficient = 1.f - leftCoefficient;
+            return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+        break;
+
+        case CurveType::CIRCINOUT:
+            if(leftCoefficient < 0.5f){
+                leftCoefficient = 0.5f * (1.f - sqrt(1.f - 4.f*(leftCoefficient*leftCoefficient)));
+            } else {
+                leftCoefficient = 0.5f * (sqrt(-1*((2*leftCoefficient)-3.f)*((2.f*leftCoefficient)-1.f)) + 1.f);
+            }
+            rightCoefficient = 1.f - leftCoefficient;
+            return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+        break;
+
+        case CurveType::ELASTICIN:
+            leftCoefficient = std::sin(13.f * piHalf * leftCoefficient) * std::pow(2.f, 10.f * (leftCoefficient - 1.f));
+            rightCoefficient = 1.f - leftCoefficient;
+            return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+        break;
+
+        case CurveType::ELASTICOUT:
+            leftCoefficient = std::sin(-13.f * piHalf * (leftCoefficient + 1.f)) * std::pow(2.f, -10.f * leftCoefficient) + 1.f;
+            rightCoefficient = 1.f - leftCoefficient;
+            return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+        break;
+
+        case CurveType::ELASTICINOUT:
+            if(leftCoefficient < 0.5f) {
+                leftCoefficient = 0.5f * std::sin(13.f * piHalf * (2.f * leftCoefficient)) * std::pow(2.f, 10.f * ((2.f * leftCoefficient) - 1.f));
+            } else {
+                leftCoefficient = 0.5f * (std::sin(-13.f * piHalf * ((2.f * leftCoefficient - 1.f) + 1.f)) * std::pow(2.f, -10.f * (2.f * leftCoefficient - 1.f)) + 2.f);
+            }
+            rightCoefficient = 1.f - leftCoefficient;
+            return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+        break;
+
+        case CurveType::BACKIN: {
+            const float s = 1.70158f;
+            leftCoefficient = leftCoefficient * leftCoefficient * ((s + 1.f) * leftCoefficient - s);
+            rightCoefficient = 1.f - leftCoefficient;
+            return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+        }
+        break;
+
+        case CurveType::BACKOUT: {
+            const float s = 1.70158f;
+            leftCoefficient -= 1.f;
+            leftCoefficient = 1.f * (leftCoefficient*leftCoefficient*((s + 1.f)*leftCoefficient + s) + 1.f);
+            rightCoefficient = 1.f - leftCoefficient;
+            return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+        }
+        break;
+
+        case CurveType::BACKINOUT:
+            if(leftCoefficient < 0.5f){
+                float f = 2*leftCoefficient;
+                leftCoefficient = 0.5f * (f * f * f - f * std::sin(f * pi) );
+                rightCoefficient = 1.f - leftCoefficient;
+                return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+            } else {
+                float f = (1.f - (2.f*leftCoefficient - 1.f));
+                leftCoefficient = 0.5f * (1.f - (f * f * f - f * std::sin(f * pi))) + 0.5f;
+                rightCoefficient = 1.f - leftCoefficient;
+                return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+            }
+        break;
+
+        case CurveType::BOUNCEIN:
+            leftCoefficient = 1.f - bounceout(1.f - leftCoefficient);
+            rightCoefficient = 1.f - leftCoefficient;
+            return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+        break;
+
+        case CurveType::BOUNCEOUT:
+            leftCoefficient = bounceout(leftCoefficient);
+            rightCoefficient = 1.f - leftCoefficient;
+            return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+        break;
+
+        case CurveType::BOUNCEINOUT:
+            if(leftCoefficient < 0.5f){
+                leftCoefficient = 0.5f * (1.f - bounceout(1.f - leftCoefficient * 2.f));
+                rightCoefficient = 1.f - leftCoefficient;
+                return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+            } else {
+                leftCoefficient = 0.5f * bounceout((leftCoefficient * 2.f -1.f)) + 0.5f;
+                rightCoefficient = 1.f - leftCoefficient;
+                return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+            }
+        break;
+
+        case CurveType::SINESQUARE:
+            leftCoefficient = std::sin((leftCoefficient) * piHalf);
+            leftCoefficient *= leftCoefficient;
+            rightCoefficient = 1.f - leftCoefficient;
+            return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+        break;
+    
+        case CurveType::EXPONENTIAL:
+            leftCoefficient = 1.f / (1.f + std::exp(6.f - 12.f * (leftCoefficient)));
+            rightCoefficient = 1.f - leftCoefficient;
+            return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+        break;
+        
+        case CurveType::SCHUBRING1:
+            leftCoefficient = 2.f * (leftCoefficient +(0.5f - leftCoefficient) * std::abs(0.5f - leftCoefficient)) - 0.5f; 
+            rightCoefficient = 1.f - leftCoefficient;
+            return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+        break;
+        
+        case CurveType::SCHUBRING2:{
+            float p1pass = 2.f * (leftCoefficient*(0.5f - leftCoefficient)*std::abs(0.5f - leftCoefficient)) - 0.5f;
+            float p2pass = 2.f * (p1pass + (0.5f - p1pass) * std::abs(0.5f - p1pass)) - 0.5f;
+            leftCoefficient = (p1pass + p2pass) / 2.f;
+            rightCoefficient = 1.f - leftCoefficient;
+            return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+        }
+        break;
+        
+        case CurveType::SCHUBRING3:{
+            double p1pass= 2.f*(leftCoefficient+(0.5f-leftCoefficient)*std::abs(0.5f-leftCoefficient))-0.5f;
+            double p2pass= 2.f*(p1pass+(0.5f-p1pass)*std::abs(0.5f-p1pass))-0.5f;
+            leftCoefficient = p2pass;
+            rightCoefficient = 1.f - leftCoefficient;
+            return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+        }
+        break;
+        
+        case CurveType::SWING:
+            leftCoefficient = ((-std::cos(pi * leftCoefficient) * 0.5f) + 0.5f);
+            rightCoefficient = 1.f - leftCoefficient;
+            return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+        break;
+        
+        case CurveType::SINPI2:
+            leftCoefficient = std::sin(leftCoefficient * piHalf);
+            rightCoefficient = 1.f - leftCoefficient;
+            return leftCoefficient * points[leftIndex].y + rightCoefficient * points[rightIndex].y;
+        break;
+        
+        default:
+            return -1.f;
+        break;
+    }
+    return -1.f;
+}
+
 void Curve::AddPoint(glm::vec2 newPoint) {
     auto iterator = points.begin();
-    
+
     for(int i = 0; i < points.size() && points[i].x < newPoint.x; i++)
         iterator++;
     points.insert(iterator++, newPoint);
