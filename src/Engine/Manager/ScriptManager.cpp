64bf14532dc609e12e7f5528696f8c442f62c3c6
Albin Bernhardsson <catscratchesp@gmail.com>
2017-05-11 15:10:57 +0200

    Expose normalize vec4

--- src/Engine/Manager/ScriptManager.cpp
+++ src/Engine/Manager/ScriptManager.cpp
@@ -204,6 +204,7 @@ ScriptManager::ScriptManager() {
     // Register GLM functions.
     engine->RegisterGlobalFunction("vec2 normalize(const vec2 &in)", asFUNCTIONPR(glm::normalize, (const glm::vec2&), glm::vec2), asCALL_CDECL);
     engine->RegisterGlobalFunction("vec3 normalize(const vec3 &in)", asFUNCTIONPR(glm::normalize, (const glm::vec3&), glm::vec3), asCALL_CDECL);
+    engine->RegisterGlobalFunction("vec4 normalize(const vec4 &in)", asFUNCTIONPR(glm::normalize, (const glm::vec4&), glm::vec4), asCALL_CDECL);
     
     // Register Entity.
     engine->RegisterObjectType("Entity", 0, asOBJ_REF | asOBJ_NOCOUNT);
