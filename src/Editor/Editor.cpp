204e9a62edaa267a9f12a1a4bf5deb6e7bcdec36
Albin Bernhardsson <catscratchesp@gmail.com>
2017-05-16 14:10:44 +0200

    Don't draw cursor using ImGui

--- src/Editor/Editor.cpp
+++ src/Editor/Editor.cpp
@@ -31,8 +31,6 @@ Editor::Editor() {
     cameraEntity = cameraWorld.CreateEntity("Editor Camera");
     cameraEntity->AddComponent<Component::Lens>();
     cameraEntity->position.z = 10.0f;
-    
-    ImGui::GetIO().MouseDrawCursor = true;
 }
 
 void Editor::Show(float deltaTime) {
