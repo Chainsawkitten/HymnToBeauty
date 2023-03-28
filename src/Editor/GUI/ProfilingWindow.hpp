0568f784acabbc30d4af8f12347e629b31c6a4f0
Chainsawkitten <catscratchesp@gmail.com>
2023-03-28 17:00:12 +0200

    Timeline statistics
    Add statistics to the timeline viewer, which shows the average time events took per frame.

--- src/Editor/GUI/ProfilingWindow.hpp
+++ src/Editor/GUI/ProfilingWindow.hpp
@@ -2,6 +2,7 @@
 
 #include <Engine/Manager/ProfilingManager.hpp>
 #include <GUI/LogView.hpp>
+#include <map>
 #include "FileSelector.hpp"
 
 namespace GUI {
@@ -42,6 +43,9 @@ class ProfilingWindow {
     void ShowThreadNames();
     void ShowThreads();
 
+    void CalculateStatistics();
+    void ShowStatistics();
+
     bool visible = false;
 
     FileSelector fileSelector;
@@ -54,5 +58,13 @@ class ProfilingWindow {
     bool resetScroll = false;
 
     std::vector<ThreadView> threadViews;
+
+    struct EventInfo {
+        std::string name;
+        double totalDuration = 0.0;
+        uint32_t count = 0;
+    };
+    std::vector<std::vector<EventInfo>> statistics;
+    uint32_t frameCount = 0;
 };
 } // namespace GUI
